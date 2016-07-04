9ca5d8da6139efce32f63ac174117efff72ceb66
Venkatramanan Krishnamani <venky.krishna@icloud.com>
2016-07-04 13:10:26 -0500

    Final Push

--- stat_maker_gui.py
+++ stat_maker_gui.py
@@ -4,6 +4,7 @@ import thread
 import glob
 import time
 import subprocess
+from datetime import datetime
 from libraries.pyper import *
 from PyQt4 import QtCore, QtGui, uic
 
@@ -55,12 +56,15 @@ class Stat_Maker_Gui(QtGui.QMainWindow, form_class):
 
         # QThread for processes
         self.process = None
-        self.run_btn.setEnabled(False)
+        # self.run_btn.setEnabled(False)
+        self.set_interaction_state(False)
+        self.verify_installation_btn.setEnabled(True)
         self.data = {}
         self.directory = '~/'
         self.jags_path = None
         self.r_path = None
         self.started = 0
+        self.r = None
 
         # thread.start_new_thread(self.monitor_files, ())
 
@@ -97,20 +101,74 @@ class Stat_Maker_Gui(QtGui.QMainWindow, form_class):
             else:
                 self.run_btn.setEnabled(False)
 
-        if not self.process == None:
-            try:
-                os.kill(self.process.pid + 1, 0)
-            except OSError:
-                self.statusbar.showMessage("Finished Installation", 8000)
-                self.started = 0
+                # if not self.process == None:
+                #     try:
+                #         os.kill(self.process.pid + 1, 0)
+                #     except OSError:
+                #         self.statusbar.showMessage("Finished Installation", 8000)
+                #         self.started = 0
 
-        if not os.path.exists('/usr/local/bin/jags') and self.started == 0:
+                # if not os.path.exists('/usr/local/bin/jags') and self.started == 0:
+                #     self.process = subprocess.Popen('open statistics/JAGS.pkg', shell=True)
+                #     self.started = 1
+
+                # if not os.path.exists('/usr/local/bin/R') and self.started == 0:
+                #     self.process = subprocess.Popen('open statistics/R.pkg', shell=True)
+                #     self.started = 1
+
+    def get_exec_path(self, executable):
+        if os.path.exists('/usr/local/bin/' + executable):
+            return True
+        if os.path.exists('/usr/bin/' + executable):
+            return True
+        return False
+
+    def check_deepn_installed(self):
+        dout = self.r("require('deepn')")
+        dout = dout.replace(' ', '')
+        dout = dout.replace('\n', '')
+        if re.match('.+nopackage.+', dout):
+            return False
+        return True
+
+    @QtCore.pyqtSlot()
+    def on_verify_installation_btn_clicked(self):  #####################   new definition
+
+        if not os.path.exists('/usr/local/bin/jags') or os.path.exists('/usr/bin/jags'):
+            self.verify_installation_btn.setText("Installing JAGS...")
             self.process = subprocess.Popen('open statistics/JAGS.pkg', shell=True)
-            self.started = 1
+            while not self.get_exec_path('jags'):
+                time.sleep(0.5)
 
-        if not os.path.exists('/usr/local/bin/R') and self.started == 0:
+        if os.path.exists('/usr/local/bin/jags'):
+            self.jags_path = '/usr/local/bin/jags'
+        if os.path.exists('/usr/bin/jags'):
+            self.jags_path = '/usr/bin/jags'
+
+        if not os.path.exists('/usr/local/bin/R') or os.path.exists('/usr/bin/R'):
+            self.verify_installation_btn.setText("Installing R...")
             self.process = subprocess.Popen('open statistics/R.pkg', shell=True)
-            self.started = 1
+            while not self.get_exec_path('R'):
+                time.sleep(0.5)
+
+        if os.path.exists('/usr/local/bin/R'):
+            self.r_path = '/usr/local/bin/R'
+        if os.path.exists('/usr/bin/R'):
+            self.r_path = '/usr/bin/R'
+
+        if os.path.exists(self.r_path):
+            self.r = R(RCMD=self.r_path)
+            if not self.check_deepn_installed():
+                self.verify_installation_btn.setText("Finishing Installation...")
+                subprocess.Popen(['bash install_deepn.sh ' + self.r_path], shell=True)
+                while not self.check_deepn_installed():
+                    time.sleep(0.5)
+
+        self.set_interaction_state(True)
+        self.run_btn.setEnabled(False)
+        self.verify_installation_btn.setEnabled(False)
+        self.verify_installation_btn.setText("Installations are Correct")
+        self.statusbar.showMessage("R, JAGS, and DEEPN Installation is Correct")
 
     def which(self, program):
         try:
@@ -295,28 +353,49 @@ class Stat_Maker_Gui(QtGui.QMainWindow, form_class):
     def runr(self):
         self.r("analyzeDeepn('" + os.path.join(self.directory, 'r_input.params') + "', outfile='" + \
                os.path.join(self.directory, 'statmaker_output.csv') + "')")
-        self.statusbar.showMessage("Saved Results to File: %s" % os.path.join(self.directory, 'statmaker_output.csv'))
+        parameters = []
+        p = open(os.path.join(self.directory, "r_input.params"))
+        for line in p:
+            parameters.append(line)
+        p.close()
+        s = open(os.path.join(self.directory, "statmaker_output.csv"))
+        for line in s:
+            parameters.append(line)
+        s.close()
+
+        FORMAT = '%d%b%Y-%H%M%S'
+        name = '%s_%s.csv' % ('statmaker_output', datetime.now().strftime(FORMAT))
+        out = open(os.path.join(self.directory, name), 'w')
+        for l in parameters:
+            out.write(l)
+        out.close()
+
         map(os.remove, glob.glob(os.path.join(self.directory, "*_temp.csv")))
+        map(os.remove, glob.glob(os.path.join(self.directory, "r_input.params")))
+        map(os.remove, glob.glob(os.path.join(self.directory, "statmaker_output.csv")))
+        self.statusbar.showMessage("Saved Results to File: %s" % os.path.join(self.directory, name))
         self.set_interaction_state(True)
 
     @QtCore.pyqtSlot()
     def on_run_btn_clicked(self):
-        self.r = R(RCMD='/usr/local/bin/R')
-        dout = self.r("require('deepn')")
-        dout = dout.replace(' ', '')
-        dout = dout.replace('\n', '')
-        if re.match('.+nopackage.+', dout):
-            self.statusbar.showMessage("DEEPN R Package Not Found. To install follow instructions in the manual.")
-        else:
-            self.statusbar.showMessage("Running DEEPN statistics... Please Wait...")
-            self.write_r_input()
-            self.set_interaction_state(False)
-            thread.start_new_thread(self.runr, ())
-            # self.process.start(self.r_path, [os.path.join(self.directory, 'sample.r')])
+        # self.r = R(RCMD='/usr/local/bin/R')
+        # dout = self.r("require('deepn')")
+        # dout = dout.replace(' ', '')
+        # dout = dout.replace('\n', '')
+        # if re.match('.+nopackage.+', dout):
+        #     self.statusbar.showMessage("DEEPN R Package Not Found. To install follow instructions in the manual.")
+        # else:
+        self.statusbar.showMessage("Running DEEPN statistics... Please Wait...")
+        self.write_r_input()
+        self.set_interaction_state(False)
+        thread.start_new_thread(self.runr, ())
+        # self.process.start(self.r_path, [os.path.join(self.directory, 'sample.r')])
+
 
 def appExit():
     app.quit()
     sys.exit()
+
 
 if __name__ == '__main__':
     form = Stat_Maker_Gui()
