2a1a2e9690fd2f419aa784966b1d8da1be50513a
emptyewer <venky.krishna@icloud.com>
2017-02-03 18:22:11 -0600

    + Bug in GCJM fixed

--- gc_jm.py
+++ gc_jm.py
@@ -12,7 +12,8 @@ blast_db_name = sys.argv[6]
 gene_count_exe = sys.argv[7]
 junction_make_exe = sys.argv[8]
 
-combined = sys.argv[9]
+gene_list_file = sys.argv[9]
+combined = sys.argv[10]
 
 subprocess.call([gene_count_exe,  main_directory, gene_dictionary, chromosomes_list_name, combined], shell=False)
-subprocess.call([junction_make_exe, main_directory, junction_sequence, exclusion_sequence, blast_db_name, combined], shell=False)
+subprocess.call([junction_make_exe, main_directory, junction_sequence, exclusion_sequence, blast_db_name, gene_list_file, combined], shell=False)
